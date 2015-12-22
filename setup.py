d7eb68b41a4c215f233215b2478bce7e483e0d63
Alex Thompson <godfoder@acis.ufl.edu>
2015-12-22 16:41:50 -0500

    fix bump version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(*paths):
 
 setup(
     name='idq',
-    version='0.4.1',
+    version='0.4.2',
     description='iDigQuality Base Module',
     long_description=read('README.rst') + "\n\n",
     url='http://github.com/idigbio/idq/',   
